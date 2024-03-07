c06106b4ffd3e85cbcd595692ecd6cb21c40bb99
omar.arenas <omar.arenas@btilab.co>
2024-03-07 13:48:56 -0500

    fix: imports with the images

--- src/Constants/Images.ts
+++ src/Constants/Images.ts
@@ -1,18 +1,5 @@
-const importAll = async (r: Record<string, any>) => {
-	const images: { [key: string]: string } = {}
-	for (const key in r) {
-		if (r.hasOwnProperty(key)) {
-			const imageName = RegExp(/([^\\/]+)\.\w+$/).exec(key)?.[1] ?? "" // Extraer el nombre del archivo sin extensión
-			const module = await r[key]()
-			images[imageName] = module.default
-		}
-	}
-	return images
-}
-
-const imageFiles = import.meta.glob("../Assets/Images/*.png")
-const imagesResolved = await importAll(imageFiles)
+import Logo from "../Assets/Images/logo.png"
 
 export const IMAGES = {
-	logo: imagesResolved["logo"],
+	logo: Logo,
 }
